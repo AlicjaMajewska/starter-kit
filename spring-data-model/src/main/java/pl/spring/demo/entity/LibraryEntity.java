964fdf112e715a3cdddf61c179d0dd4885dcc570
AlicjaMajewska <alicja.majewska94@gmail.com>
2015-08-11 12:25:13 +0200

    LibraryEntity half done

---
+++ spring-data-model/src/main/java/pl/spring/demo/entity/LibraryEntity.java
@@ -0,0 +1,67 @@
+package pl.spring.demo.entity;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+@Entity
+@Table(name = "LIBRARY")
+public class LibraryEntity {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	
+	 @Column(nullable = false, length = 50)
+	 private String title;
+}
+
+//package pl.spring.demo.entity;
+//
+
+//
+
+//public class BookEntity implements Serializable {
+    
+//    @Column(nullable = false, length = 50)
+//    private String title;
+//
+//    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @JoinTable(name = "BOOK_AUTHOR",
+//            joinColumns = {@JoinColumn(name = "BOOK_ID", nullable = false, updatable = false)},
+//            inverseJoinColumns = {@JoinColumn(name = "AUTHOR_ID", nullable = false, updatable = false)}
+//    )
+//    private Set<AuthorEntity> authors = new HashSet<>();
+//
+//    // for hibernate
+//    protected BookEntity() {
+//}
+//
+//    public BookEntity(Long id, String title) {
+//        this.id = id;
+//        this.title = title;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public Set<AuthorEntity> getAuthors() {
+//        return authors;
+//    }
+//
+//    public void setAuthors(Set<AuthorEntity> authors) {
+//        this.authors = authors;
+//    }
+//}
